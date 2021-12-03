7f1ffdd84f9272158a2170da3abfd2e6d72bc751
SergeyIgnatyuk <signatuk89@gmail.com>
2021-12-03 16:21:50 +0300

    test

--- bot-backend.js
+++ bot-backend.js
@@ -1,19 +1,22 @@
-var token = "your bot token goes here"; 
+var token = "2136035455:AAGLWxJ-JUAAImnVob6r_e3r2gLa2Ky80BE"; 
 var telegramUrl = "https://api.telegram.org/bot" + token;
 
-const DEFAULT_INTRO_TEXT = "Перешлите сообщение от канала (или группы) и бот ответит, находится ли он в списке.\nОбратите внимание, что сообщение может быть в канале за авторством кого-то из пользователей, в таком случае бот вернет ошибку, так как в пересланном сообщении не будет содержаться информация о канале, из которого оно было переслано."
-const DEFAULT_ENTRY_FOUND_TEXT = "\nКанал/группа находится в списке!"
-const DEFAULT_ENTRY_NOT_FOUND_TEXT = "\nКанал/группа не находится в списке."
+const DEFAULT_INTRO_TEXT = "Перешлите сообщение от канала (или группы) и бот ответит, находится ли он в списке.\nОбратите внимание, что сообщение может быть в канале за авторством кого-то из пользователей, в таком случае бот вернет ошибку, так как в пересланном сообщении не будет содержаться информация о канале, из которого оно было переслано.";
+const DEFAULT_ENTRY_FOUND_TEXT = "\nКанал/группа находится в списке!";
+const DEFAULT_ENTRY_NOT_FOUND_TEXT = "\nКанал/группа не находится в списке.";
 
 // default texts will be replaced by corresponding values from configuration if set
-var introText = DEFAULT_INTRO_TEXT
-var entryFoundText = DEFAULT_ENTRY_FOUND_TEXT
+var introText = DEFAULT_INTRO_TEXT;
+var entryFoundText = DEFAULT_ENTRY_FOUND_TEXT;
 var entryNotFoundText = DEFAULT_ENTRY_NOT_FOUND_TEXT;
 
 // some initialization code goes here
 (function() {
   var configSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Configuration")
   var configData = configSheet.getDataRange().getValues()
+
+
+
 
   for(var i = 0; i < configData.length; i++) {
       if (configData[i][0] == "INTRO_TEXT" && configData[i][0] != "") {
@@ -41,6 +44,12 @@ function sendMessage(chat_id, text) {
   Logger.log(response.getContentText()); 
 }
 
+function getChat(chat_id) {
+  var url = telegramUrl + "/getChat?chat_id=" + chat_id;
+  var response = UrlFetchApp.fetch(url);
+  return JSON.parse(response.getContentText());
+}
+
 function flatten(arrayOfArrays) {
   return [].concat.apply([], arrayOfArrays);
 }
@@ -58,6 +67,7 @@ function doPost(e) {
   var forward_from_chat = contents.message.forward_from_chat;
   var forward_from = contents.message.forward_from;
   var forward_sender_name = contents.message.forward_sender_name;
+  var entities = contents.message.entities;
   // Logger.log("forward_from_chat id = " + forward_from_chat.id);
   if (forward_from_chat) {
     text = "Переслано сообщение от \n";
@@ -76,9 +86,25 @@ function doPost(e) {
     text = "Переслано сообщение от обычного пользователя:\n"
     text += JSON.stringify(forward_sender_name);
     text += "\nБот проверяет только каналы и группы на нахождение в списке."
+  } else if (entities) {
+    if (contents.message.text.substring(13, 21) == "joinchat") {
+        var hash = contents.message.text.substring(22);
+      text = "Id канала: \n";
+      text += JSON.stringify(contents);
+    } else {
+        var id = getChat("@" + contents.message.text.substring(13)).result.id;
+        text = "Id канала: \n";
+        text += JSON.stringify(id);
+        text += JSON.stringify(contents);
+    }
+    
+    if (ids.includes(id)) {
+      text += "\n" + entryFoundText;
+    } else {
+      text += "\n" + entryNotFoundText;
+    }
   }
 
-  // Logger.log(text);
-
+  //Logger.log(text);
   sendMessage(chat_id,text)
 }
